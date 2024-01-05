657ff021215a8373baeb08678ae043afd6071f53
data-piper <mujtabaali22@gmail.com>
2024-01-05 16:18:40 -0600

    rebuild

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function app() {
+  return (
+    <SafeAreaView style={{ flex:0, backgroundColor: 'red' }}>
+      <Text>
+        Morvis
+      </Text>
+    </SafeAreaView>
+  )
+}
